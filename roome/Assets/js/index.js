fda4a6930b8a5404d3da2481e12c43d447827d76
Marko Simundza <markosimundza@gmail.com>
2017-02-01 18:48:26 +0100

    roome latest and greatest

--- roome/Assets/js/index.js
+++ roome/Assets/js/index.js
@@ -1,43 +1,27 @@
 $(function() {
-    $.fn.visible = function(partial) {
+    $(".fancybox").fancybox();
 
-        var $t = $(this),
-            $w = $(window),
-            viewTop = $w.scrollTop(),
-            viewBottom = viewTop + $w.height(),
-            _top = $t.offset().top,
-            _bottom = _top + $t.height(),
-            compareTop = partial === true ? _bottom : _top,
-            compareBottom = partial === true ? _top : _bottom;
+    $(window).on("load resize", function() {
+        // var windowWidth = window.innerWidth;
 
-        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+        // if (windowWidth < 768) {
+        //     var firstSection = $('.first-section');
+        //     var preRegElemMargin = $('.pre-register-image-container');
 
-    };
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            console.log(target)
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            console.log(target)
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 500);
-                return false;
-            }
-        }
-    });
+        //     var newval = parseInt(windowWidth - 200);
+        //     console.log(newval)
+        //         // $('.container').css('margin-top',newval);
 
-    $(window).on("load resize scroll", function() {
-        // get all elements to animate
-        var elem = $('.emerge');
-        $.each(elem, function(i, el) {
-            var el = $(el);
-            if (el.visible(true)) {
-                el.addClass('animated fadeInUp');
-            }
-        });
+        //     //         console.log(firstSection);
+        //     //         console.log(preRegElemMargin);
+        //     //         console.log(window.innerWidth);
+
+        //     // currently 500
+        //     preRegElemMargin.css('margin-top', -newval);
+        //     // currently 650
+        //     firstSection.height(newval);
+        // }
     });
 
 
